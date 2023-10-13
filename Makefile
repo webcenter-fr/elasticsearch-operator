b3dbc8d8022c35c7f323209a48779df12b2ecec7
disaster37 <linuxworkgroup@hotmail.com>
2023-10-13 09:12:03 +0000

    feat: exclude generated file from coverage

--- Makefile
+++ Makefile
@@ -104,7 +104,8 @@ vet: ## Run go vet against code.
 
 .PHONY: test
 test: manifests generate fmt envtest ## Run tests.
-	KUBEBUILDER_ASSETS="$(shell $(ENVTEST) use $(ENVTEST_K8S_VERSION) -p path)" TEST=true go test -p 1 -v -coverprofile cover.out -timeout 1200s -count 1 -covermode=atomic ./apis/... ./pkg/... ./controllers/...  $(TESTARGS)
+	KUBEBUILDER_ASSETS="$(shell $(ENVTEST) use $(ENVTEST_K8S_VERSION) -p path)" TEST=true go test -p 1 -v -coverprofile cover.out.tmp -timeout 1200s -count 1 -covermode=atomic ./apis/... ./pkg/... ./controllers/...  $(TESTARGS)
+	cat cover.out.tmp | grep -v "_generated*.go" > cover.out
 
 .PHONY: test-acc
 test-acc:
---
+++ apis/beat/v1/suite_test.go
@@ -0,0 +1,115 @@
+package v1
+
+import (
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/suite"
+	cerebrocrd "github.com/webcenter-fr/elasticsearch-operator/apis/cerebro/v1"
+	elasticsearchcrd "github.com/webcenter-fr/elasticsearch-operator/apis/elasticsearch/v1"
+	kibanacrd "github.com/webcenter-fr/elasticsearch-operator/apis/kibana/v1"
+	kibanaapicrd "github.com/webcenter-fr/elasticsearch-operator/apis/kibanaapi/v1"
+	logstashcrd "github.com/webcenter-fr/elasticsearch-operator/apis/logstash/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	//+kubebuilder:scaffold:imports
+	//+kubebuilder:scaffold:imports
+)
+
+var testEnv *envtest.Environment
+
+type TestSuite struct {
+	suite.Suite
+	k8sManager manager.Manager
+}
+
+func TestBeatApiSuite(t *testing.T) {
+	suite.Run(t, new(TestSuite))
+}
+
+func (t *TestSuite) SetupSuite() {
+
+	logf.SetLogger(zap.New(zap.UseDevMode(true)))
+	logrus.SetLevel(logrus.TraceLevel)
+	logrus.SetFormatter(&logrus.TextFormatter{
+		DisableQuote: true,
+	})
+
+	// Setup testenv
+	testEnv = &envtest.Environment{
+		CRDDirectoryPaths: []string{
+			filepath.Join("../../..", "config", "crd", "bases"),
+		},
+		ErrorIfCRDPathMissing:    true,
+		ControlPlaneStopTimeout:  120 * time.Second,
+		ControlPlaneStartTimeout: 120 * time.Second,
+	}
+	cfg, err := testEnv.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	// Add CRD sheme
+	err = scheme.AddToScheme(scheme.Scheme)
+	if err != nil {
+		panic(err)
+	}
+
+	err = elasticsearchcrd.AddToScheme(scheme.Scheme)
+	if err != nil {
+		panic(err)
+	}
+	err = kibanacrd.AddToScheme(scheme.Scheme)
+	if err != nil {
+		panic(err)
+	}
+	err = logstashcrd.AddToScheme(scheme.Scheme)
+	if err != nil {
+		panic(err)
+	}
+	err = AddToScheme(scheme.Scheme)
+	if err != nil {
+		panic(err)
+	}
+	err = cerebrocrd.AddToScheme(scheme.Scheme)
+	if err != nil {
+		panic(err)
+	}
+	err = kibanaapicrd.AddToScheme(scheme.Scheme)
+	if err != nil {
+		panic(err)
+	}
+
+	// Init k8smanager and k8sclient
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme: scheme.Scheme,
+	})
+	if err != nil {
+		panic(err)
+	}
+	t.k8sManager = k8sManager
+}
+
+func (t *TestSuite) TearDownSuite() {
+
+	// Teardown the test environment once controller is fnished.
+	// Otherwise from Kubernetes 1.21+, teardon timeouts waiting on
+	// kube-apiserver to return
+	err := testEnv.Stop()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (t *TestSuite) BeforeTest(suiteName, testName string) {
+
+}
+
+func (t *TestSuite) AfterTest(suiteName, testName string) {
+}
